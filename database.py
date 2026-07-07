5ad04a515c3888068c2ece9d6422d998fdad5d28
Atul Patle <atulpatle006@gmail.com>
2026-07-07 23:29:09 +0530

    apply async non-blocking concepts

--- database.py
+++ database.py
@@ -7,3 +7,20 @@ engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
+
+
+# """async non-blocking """
+
+# from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+# from sqlalchemy.orm import sessionmaker, declarative_base
+
+# DB_URL = "mysql+pymysql://root:Atul%402006@localhost/e_comm"
+
+# engine = create_async_engine(DB_URL)
+# SessionLocal = sessionmaker(
+#                 bind=engine,
+#                 class_ = AsyncSession,
+#                 expire_on_commit=False
+#                 )
+
+# Base = declarative_base()
