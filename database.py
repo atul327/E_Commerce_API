a41fdd51fe69028175783b3bd6b7cca0b3d5c861
Atul Patle <atulpatle006@gmail.com>
2026-07-09 00:29:10 +0530

    converted into async

--- database.py
+++ database.py
@@ -1,26 +1,26 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base, Session
-
-DB_URL = "mysql+pymysql://root:Atul%402006@localhost/e_comm"
-
-engine = create_engine(DB_URL)
-SessionLocal = sessionmaker(bind=engine)
-
-Base = declarative_base()
-
-
-# """async non-blocking """
-
-# from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-# from sqlalchemy.orm import sessionmaker, declarative_base
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker, declarative_base, Session
 
 # DB_URL = "mysql+pymysql://root:Atul%402006@localhost/e_comm"
 
-# engine = create_async_engine(DB_URL)
-# SessionLocal = sessionmaker(
-#                 bind=engine,
-#                 class_ = AsyncSession,
-#                 expire_on_commit=False
-#                 )
+# engine = create_engine(DB_URL)
+# SessionLocal = sessionmaker(bind=engine)
 
 # Base = declarative_base()
+
+
+"""async non-blocking """
+
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DB_URL = "mysql+asyncmy://root:Atul%402006@localhost/e_comm"
+
+engine = create_async_engine(DB_URL)
+AsyncSessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+Base = declarative_base()
